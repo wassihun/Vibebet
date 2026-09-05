7aaffbd135e7276bcbaa6e8c97950f440e5cfcc7
wassihun <wassihun2381@gmail.com>
2026-09-05 17:02:58 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'betting_db', // የዳታቤዝዎ ስም 'afrobet' ከሆነ ይሄንን ወደ 'afrobet' ይቀይሩት
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    connectTimeout: 20000 // Aiven ላይ ዘግይቶ እንዳይቋረጥ
 });
 
 module.exports = pool;
